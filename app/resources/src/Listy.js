cd1605628ecc94bdd08d308ab747cb92772aed28
Tina <tmorozco1990@gmail.com>
2018-04-11 15:07:09 -0700

    cleaning up styles.css and adding notes

--- app/resources/src/Listy.js
+++ app/resources/src/Listy.js
@@ -5,15 +5,18 @@
 // desc: controller for app.js
 
 /** an obj to hold the list information for Listy and display it in the html
- * @param {array} list this is an empty array to add item objects to
+ * @param {String} staticCacheName this is the name of the static cache from sw.js. does not apply in electron version.
+ * @param {Array} list this is an empty array to add item objects to
  * @method _displayItem(item) this method takes in the item obj and displays it in the ul. only used by other methods
  * @method addItem(ListItem) this is the main method that takes in the list item, adds it to the html template
  * then pushes the template to the 'list' array and displays the item.
  * @method addClick(item) this method simulates a click and pulls input from the user as the parameter 'item'
  */
-module.exports = Listy = {
+module.exports = Listy =
+{
     staticCacheName: '', // cache (still used in non-electron build)
     list: [],
+    // method not used in the election version at this time
     _registerSvcWorker: function () {
         'use strict';
         if ('serviceWorker' in navigator) {
@@ -45,11 +48,15 @@ module.exports = Listy = {
         this._displayItem(item);  // display the item
     },
     addClick: function (item) {
+        // set listItem to be the user input value
         var listItem = $('input:text').val();
 
+        // add item
         if (listItem)
-            Listy.addItem(listItem);
-        $('input:text').val('');
+            Listy.addItem(listItem)
+
+        // clear input and return from addClick()
+        $('input:text').val('')
         return
     }
 };
